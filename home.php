4bc4145521c21c4a4a6ddda1b0c8d8f032adaab7
TuyanJamamim0599 <113906425+TuyanJamamim@users.noreply.github.com>
2022-12-12 16:00:08 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php 
+	session_start();
+
+	if(isset($_COOKIE['status']))
+	{
+		if($_SESSION['userType'] == "Admin")
+		{
+			header('location: homeAdmin.php');
+		}
+		else
+		{
+			header('location: homeUser.php');
+		}
+	}
+	else
+	{
+		echo "invalid request";
+	} 
+
+?>
